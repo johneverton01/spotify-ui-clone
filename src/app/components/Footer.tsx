d49e13f154231e4d549c835fbf6169f3cdc14019
John Everton <johneverton44@gmail.com>
2023-05-08 10:55:43 -0300

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,46 @@
+import { Laptop2, LayoutList, Maximize2, Mic2, Play, Repeat, Shuffle, SkipBack, SkipForward, Volume } from "lucide-react";
+import Image from "next/image";
+
+export function Footer() {
+  return (
+    <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/album.jpeg" width={56} height={56} alt=""/>
+          <div className="flex flex-col">
+            <strong className="font-normal">Where is the love?</strong>
+            <span className="text-xs text-zinc-400">Black Eyed Peas</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+            <button className="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black ml-auto pl-1">
+              <Play fill='bg-black' />
+            </button>
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-zinc-400 text-xs">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full" />
+            </div>
+            <span className="text-zinc-400 text-xs">2:14</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className="flex items-center gap-2">
+            <Volume size={20}/>
+            <div className="h-1 rounded-full w-20 bg-zinc-600">
+              <div className="bg-zinc-200 w-11 h-1 rounded-full" />
+            </div>
+          </div>
+          <Maximize2 size={20} />
+        </div>
+      </footer>
+  )
+}
---
+++ src/app/components/SideBar.tsx
@@ -0,0 +1,58 @@
+import { HomeIcon, Library, Search } from "lucide-react";
+
+export function SideBar() {
+  return (
+    <aside className="w-72 bg-zinc-950 p-6">
+      <nav className="space-y-5">
+        <a
+          href=""
+          className="flex items-center gap-3 text-semibold text-sm text-zinc-200"
+        >
+          <HomeIcon />
+          Home
+        </a>
+        <a
+          href=""
+          className="flex items-center gap-3 text-semibold text-sm text-zinc-200"
+        >
+          <Search />
+          Search
+        </a>
+        <a
+          href=""
+          className="flex items-center gap-3 text-semibold text-sm text-zinc-200"
+        >
+          <Library />
+          Your Library
+        </a>
+      </nav>
+
+      <nav className="mt-10 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+        <a
+          href=""
+          className="text-sm text-zinc-400 hover:text-zinc-100 transition-colors"
+        >
+          Hot Hits Brasil
+        </a>
+        <a
+          href=""
+          className="text-sm text-zinc-400 hover:text-zinc-100 transition-colors"
+        >
+          Aniver Funk
+        </a>
+        <a
+          href=""
+          className="text-sm text-zinc-400 hover:text-zinc-100 transition-colors"
+        >
+          My Playlist #13
+        </a>
+        <a
+          href=""
+          className="text-sm text-zinc-400 hover:text-zinc-100 transition-colors"
+        >
+          Top Brasil
+        </a>
+      </nav>
+    </aside>
+  );
+}
